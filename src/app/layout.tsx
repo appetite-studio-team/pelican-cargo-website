d7199d89c7795c4f53b548c6316efb110d029951
4samad <smd2k15@gmail.com>
2025-03-14 15:00:05 +0530

    :bento: add website icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Pelican Transport" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
